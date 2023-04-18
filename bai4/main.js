be5346ac30bb6aefe7a97e66b982eedeb69a9ad9
binh-20cm <binh1892001@gmail.com>
2023-04-18 11:35:10 +0700

    upload bai test

--- bai4/main.js
+++ bai4/main.js
@@ -2,7 +2,7 @@ let arr = [3, 25, 38, 49, 12];
 // Cách 1: sử dụng hàm sort()
 arr.sort((a, b) => b - a);
 console.log(arr);
-// Cách 2: bubble sort
+// Cách 2: bubble sort không sử dụng hàm sort()
 for (let i = 0; i < arr.length; i++) {
   for (let j = i + 1; j < arr.length; j++) {
     if (arr[i] < arr[j]) {
