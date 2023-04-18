5eea307f75a79d0c7cc060e8d8a3803bb875f428
binh-20cm <binh1892001@gmail.com>
2023-04-18 11:34:07 +0700

    upload text JS Basic

--- bai4/main.js
+++ bai4/main.js
@@ -1,6 +1,8 @@
 let arr = [3, 25, 38, 49, 12];
-
-// Cách 1: bubble sort
+// Cách 1: sử dụng hàm sort()
+arr.sort((a, b) => b - a);
+console.log(arr);
+// Cách 2: bubble sort
 for (let i = 0; i < arr.length; i++) {
   for (let j = i + 1; j < arr.length; j++) {
     if (arr[i] < arr[j]) {
@@ -8,21 +10,6 @@ for (let i = 0; i < arr.length; i++) {
       arr[i] = arr[j];
       arr[j] = temp;
     }
-  }
-}
-console.log(arr);
-// Cách 2: selection sort
-for (let i = 0; i < arr.length; i++) {
-  let index = i;
-  for (let j = i + 1; j < arr.length; j++) {
-    if (arr[j] > arr[index]) {
-      index = j;
-    }
-  }
-  if (i !== index) {
-    let temp = arr[i];
-    arr[i] = arr[index];
-    arr[index] = temp;
   }
 }
 console.log(arr);
